d7f853f1eeb62f4e008d864b3008530297fcb7cb
Daniel Himmelein <daniel.himmelein@esrlabs.com>
2016-03-10 17:24:47 +0100

    Stupid mistake

--- src/mindroid/util/Assert.cpp
+++ src/mindroid/util/Assert.cpp
@@ -21,120 +21,120 @@ namespace mindroid {
 
 const char* const Assert::LOG_TAG = "Assert";
 
-void Assert::assertTrue(const char* message, bool condition, const char* file, long line) {
+void Assert::assertTrue(const char* message, bool condition) {
 	if (!condition) {
-		Log::wtf(LOG_TAG, "%s: %d (%s:%d)", message, condition, file, line);
+		Log::wtf(LOG_TAG, "%s (%d)", message, condition);
 	}
 	assert(condition);
 }
 
-void Assert::assertFalse(const char* message, bool condition, const char* file, long line) {
+void Assert::assertFalse(const char* message, bool condition) {
 	if (condition) {
-		Log::wtf(LOG_TAG, "%s: %d (%s:%d)", message, condition, file, line);
+		Log::wtf(LOG_TAG, "%s (%d)", message, condition);
 	}
 	assert(!condition);
 }
 
-void Assert::assertNull(const char* message, void* ptr, const char* file, long line) {
+void Assert::assertNull(const char* message, void* ptr) {
 	if (ptr != nullptr) {
-		Log::wtf(LOG_TAG, "%s: 0x%x (%s:%d)", message, ptr, file, line);
+		Log::wtf(LOG_TAG, "%s (0x%x)", message, ptr);
 	}
 	assert(ptr == nullptr);
 }
 
-void Assert::assertNull(const char* message, const sp<Object>& object, const char* file, long line) {
+void Assert::assertNull(const char* message, const sp<Object>& object) {
 	if (object != nullptr) {
-		Log::wtf(LOG_TAG, "%s: 0x%x (%s:%d)", message, object.getPointer(), file, line);
+		Log::wtf(LOG_TAG, "%s (0x%x)", message, object.getPointer());
 	}
 	assert(object == nullptr);
 }
 
-void Assert::assertNotNull(const char* message, void* ptr, const char* file, long line) {
+void Assert::assertNotNull(const char* message, void* ptr) {
 	if (ptr == nullptr) {
-		Log::wtf(LOG_TAG, "%s: 0x%x (%s:%d)", message, ptr, file, line);
+		Log::wtf(LOG_TAG, "%s (0x%x)", message, ptr);
 	}
 	assert(ptr != nullptr);
 }
 
-void Assert::assertNotNull(const char* message, const sp<Object>& object, const char* file, long line) {
+void Assert::assertNotNull(const char* message, const sp<Object>& object) {
 	if (object == nullptr) {
-		Log::wtf(LOG_TAG, "%s: 0x%x (%s:%d)", message, object.getPointer(), file, line);
+		Log::wtf(LOG_TAG, "%s (0x%x)", message, object.getPointer());
 	}
 	assert(object != nullptr);
 }
 
-void Assert::assertEquals(const char* message, bool expected, bool actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, bool expected, bool actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, int8_t expected, int8_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, int8_t expected, int8_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, uint8_t expected, uint8_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, uint8_t expected, uint8_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, int16_t expected, int16_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, int16_t expected, int16_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, uint16_t expected, uint16_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, uint16_t expected, uint16_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, int32_t expected, int32_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, int32_t expected, int32_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, uint32_t expected, uint32_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, uint32_t expected, uint32_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%d, %d)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, int64_t expected, int64_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, int64_t expected, int64_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%lld, %lld)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, uint64_t expected, uint64_t actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, uint64_t expected, uint64_t actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: %d != %d (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (%lld, %lld)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::assertEquals(const char* message, void* expected, void* actual, const char* file, long line) {
+void Assert::assertEquals(const char* message, void* expected, void* actual) {
 	if (expected != actual) {
-		Log::wtf(LOG_TAG, "%s: 0x%x != 0x%x (%s:%d)", message, expected, actual, file, line);
+		Log::wtf(LOG_TAG, "%s (0x%x, 0x%x)", message, expected, actual);
 	}
 	assert(expected == actual);
 }
 
-void Assert::fail(const char* message, const char* file, long line) {
-	Log::wtf(LOG_TAG, "%s (%s:%d)", message, file, line);
+void Assert::fail(const char* message) {
+	Log::wtf(LOG_TAG, "%s", message);
 	assert(false);
 }
 
